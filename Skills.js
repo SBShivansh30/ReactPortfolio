f0b85606ae9801fd5fe416a05a60da32b27948fd
ShivanshBansal <50920041+SBShivansh30@users.noreply.github.com>
2023-10-02 15:48:03 +0530

    Create Skills.js

---
+++ Skills.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <section className="skills bg-gray-100 p-4">
+      <h2 className="text-xl font-semibold mb-4">Skills</h2>
+      <ul className="list-disc ml-6">
+        <li>Python</li>
+        <li>Pyspark</li>
+        <li>SQL</li>
+        <li>KDB+</li>
+        <li>C++</li>
+        <li>C#</li>
+        <li>Microsoft Excel</li>
+        <li>Hadoop HDFS</li>
+        <li>UNIX/LINUX</li>
+        <li>DBMS</li>
+        <li>Algorithmic Trading</li>
+        {/* Add more skills as needed */}
+      </ul>
+    </section>
+  );
+};
+
+export default Skills;
