a498310f45d252ae83378388b662188a0430e59d
ShivanshBansal <50920041+SBShivansh30@users.noreply.github.com>
2023-10-02 15:45:43 +0530

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-white p-4">
+      <p>Email: your@email.com</p>
+      <p>LinkedIn: <a href="#">LinkedIn Profile</a></p>
+      <p>GitHub: <a href="#">GitHub Profile</a></p>
+    </footer>
+  );
+};
+
+export default Footer;
