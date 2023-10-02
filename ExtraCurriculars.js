0d88db94410d9554b996445081e7ae615bfae705
ShivanshBansal <50920041+SBShivansh30@users.noreply.github.com>
2023-10-02 15:46:02 +0530

    Create ExtraCurriculars.js

---
+++ ExtraCurriculars.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ExtraCurriculars = () => {
+  return (
+    <section className="extra-curriculars bg-gray-100 p-4">
+      <h2 className="text-xl font-semibold mb-4">Extra-Curriculars</h2>
+      <ul className="list-disc ml-6">
+        <li>3+ years of Personal Swing Trading experience while actively following market</li>
+        <li>Others: Badminton, Poetry, Photography</li>
+        {/* Add more extra-curricular activities as needed */}
+      </ul>
+    </section>
+  );
+};
+
+export default ExtraCurriculars;
